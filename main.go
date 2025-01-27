8fc65ce18e7786a32f7b38272f8ccfce91d116d7
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-27 11:30:16 +0530

    Created go modules,import packages and running exported functions in main

--- main.go
+++ main.go
@@ -1,54 +1,9 @@
 package main
 
 import (
+	"Go-Tutorial/basics"
+)
 
-	"fmt"
-	"time"
-	"sync"
-) 
-
-func main() {
-	var name string = "Abinesh"
-	var age int = 22
-	fmt.Printf("Name: %s\nAge: %d\n", name, age)
-
-	game := "RDR"
-	rating := 10
-	fmt.Printf("Game: %s\nRating: %d\n", game, rating)
-    
-	if rating >=10 {
-		fmt.Println("Best game")
-	} else {
-		fmt.Println("Not best game")
-	}
-
-	var wg sync.WaitGroup
-
-	for i :=0;i<50;i++ {
-		wg.Add(1)
-		go printNumber(i, &wg)
-	}
-	wg.Wait()
-
-
-	fmt.Println(add(5, 6))
-
-	p := Person{"Jhon Wick", 52}
-	fmt.Printf("Name: %s\nAge: %d\n", p.Name, p.Age)
-}
-
-func add(a int, b int) int {
-	return a+b
-}
-
-type Person struct {
-	Name string
-	Age int
-}
-
-func printNumber (n int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(1 *time.Second)
-	time.Sleep(time.Duration(n))
-	fmt.Println(n)
+func main () {
+	basics.Run_tutorial()
 }
