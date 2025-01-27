132353a274b3b240909675b736ebe321763b7868
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-27 10:54:24 +0530

    Created condition and loop in go

--- main.go
+++ main.go
@@ -10,4 +10,17 @@ func main() {
 	var age int = 22
 	fmt.Printf("Name: %s\nAge: %d\n", name, age)
 
+	game := "RDR"
+	rating := 10
+	fmt.Printf("Game: %s\nRating: %d\n", game, rating)
+    
+	if rating >=10 {
+		fmt.Println("Best game")
+	} else {
+		fmt.Println("Not best game")
+	}
+
+	for i:=0;i<5;i++ {
+		fmt.Println(i)
+	}
 }
